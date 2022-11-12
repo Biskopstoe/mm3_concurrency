05257d95d0d3b93ed84a4075cea006de2bcffb5b
Biskopstoe <s215788@dtu.dk>
2022-11-12 16:39:54 +0100

    ehh

--- main.c
+++ main.c
@@ -4,3 +4,29 @@ int main() {
     printf("Hello, World!\n");
     return 0;
 }
+
+int wordCount(){
+//look after space " "
+    int wordCount = 0;
+
+
+
+    return wordCount;
+}
+
+int charCount(){
+
+int charCount = 0;
+
+//look through all char
+    if (charCount == NULL) {
+         printf("There are no characters");
+    }
+    else {
+        for(int i=0; i<=5; i++) {
+
+        }
+    }
+
+    return charCount;
+}
