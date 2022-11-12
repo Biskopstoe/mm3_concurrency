4e1e871233d3fbca676cac91de54db62a09ddaba
Biskopstoe <s215788@dtu.dk>
2022-11-12 16:40:09 +0100

    Changes by Jacki

--- main.c
+++ main.c
@@ -1,7 +1,53 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
 
-int main() {
-    printf("Hello, World!\n");
+
+#define MAX_LINES 100
+#define MAX_LEN 1000
+
+int main()
+{
+    pthread_t t1;
+
+    //Declared ints
+    int wordCount = 0;
+    int charCount = 0;
+    int lineCount = 0;
+
+    //sets up a 2D array
+    char data[MAX_LINES][MAX_LEN];
+
+    //makes it so we work in the right directory
+    chdir("..");
+
+    //open text file
+    FILE *file;
+    file = fopen("tekst.txt", "r");
+
+    //check if the file is loaded correctly
+    if (file == NULL)
+    {
+        printf("Error opening file.\n");
+        return 1;
+    }
+
+    //reads the text file and adds it to the 2D array sorted by the current line.
+    while (!feof(file) && !ferror(file))
+        if (fgets(data[lineCount], MAX_LEN, file) != NULL)
+            lineCount++;
+
+    // Close the file when we are done working with it.
+    fclose(file);
+
+    //prints the result in terminal from the 2D array
+    for (int i = 0; i < lineCount; i++)
+        printf("%s", data[i]);
+
+    //observer thread that checks for everything is done and prints it all.
+
     return 0;
 }
 
