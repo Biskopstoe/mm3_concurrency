3b7c0909858d3fbed41c56ce4e2245d01c9ba5e0
Mads Jønsson <madspljoensson@gmail.com>
2022-11-21 14:14:48 +0100

    Added num of sentences

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #include <semaphore.h>
 
 
-#define MAX_LINES 100
+#define MAX_LINES 1000
 #define MAX_LEN 1000
 
 int charCounter(char data[MAX_LINES][MAX_LEN], int lineLengthArr[MAX_LINES], int lineCount);
@@ -21,11 +21,7 @@ FILE *fp;
 
 pthread_mutex_t lock;
 
-/**
- * the thread itself, with pointers to the data struct.
- * @param arg
- * @return
- */
+
 void *wordCountThread(void *args) {
 
     pthread_mutex_lock(&lock);
@@ -45,8 +41,28 @@ void *wordCountThread(void *args) {
 
     pthread_mutex_unlock(&lock);
 
-    sleep(10);
-    fputs("Test0 \n", fp);
+    return NULL;
+
+}
+
+void *numberOfSentences(void *args) {
+
+    pthread_mutex_lock(&lock);
+
+    int sentenceCount = 0;
+    for (int i = 0; i < lineCount; i++) { //for each line
+        for (int j = 0; j < lineLengthArr[i] + 1; j++) { //for each char
+            if (data[i][j] == 46) { //checks for spacing and end of line
+                sentenceCount++;
+            }
+        }
+    }
+    printf("Number of sentences %d \n", sentenceCount);
+
+    fprintf(fp, "Number of sentences %d \n", sentenceCount);
+
+
+    pthread_mutex_unlock(&lock);
 
     return NULL;
 
@@ -68,8 +84,6 @@ void *charCountThread(void *arg) {
     printf("Number of chars %d \n", charCount);
 
     fprintf(fp, "Number of chars %d \n", charCount);
-    sleep(1);
-    fputs("Test1 \n", fp);
 
     pthread_mutex_unlock(&lock);
 
@@ -79,10 +93,6 @@ void *charCountThread(void *arg) {
 
 int main()
 {
-    //Declared ints
-    int wordCount = 0;
-    int charCount = 0;
-
 
     /** Load file*/
     //makes it so we work in the right directory
@@ -90,7 +100,7 @@ int main()
 
     //open text file
     FILE *file;
-    file = fopen("tekst.txt", "r");
+    file = fopen("tekst1.txt", "r");
 
     //check if the file is loaded correctly
     if (file == NULL)
@@ -128,21 +138,21 @@ int main()
 
 
     //prints the result in terminal from the 2D array
-    for (int i = 0; i < lineCount; i++) {
-        printf("%s\n", data[i]);
-    }
+    //for (int i = 0; i < lineCount; i++) {
+    //    printf("%s\n", data[i]);
+    //}
 
     // file
     fp = fopen("output.txt", "w");
 
     //Multi_threading
-    pthread_t thread1, thread2; // creating separate threads.
+    pthread_t thread1, thread2, thread3; // creating separate threads.
 
     //pthread_t *pthreads[] = {&tid0}; //array containing all threads
 
     pthread_create(&thread1, NULL, wordCountThread, NULL);
     pthread_create(&thread2, NULL, charCountThread, NULL);
-
+    pthread_create(&thread3, NULL, numberOfSentences, NULL);
 
 
 
@@ -151,9 +161,7 @@ int main()
      */
     pthread_join(thread1, NULL);
     pthread_join(thread2, NULL);
-
-
-    fputs("Test2 \n", fp);
+    pthread_join(thread3, NULL);
 
     fclose(fp);
 
